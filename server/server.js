5aacf38057b4da206fa4cd49fae431202b50238d
shimroz <shimrozahemed.ngp@gmail.com>
2018-07-06 19:05:04 +0530

    new changes in chat app

--- server/server.js
+++ server/server.js
@@ -6,31 +6,36 @@ const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 var port = process.env.PORT || 3000
 
+const {generateMessage} = require('./utils/message');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 io.on('connection', (socket) =>{
   console.log('New User connected');
-  
+  socket.emit('newMessage',{
+  	from: 'Admin',
+  	text: 'Welcome to chat room',
+  	createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined chat room'));
+
   socket.on('createMessage', function(msg){
     console.log('new message created', msg);
-    io.emit('newMessage', {
-    	from: msg.from,
-    	text: msg.text,
-    	createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage(msg.from, msg.text));
+    // socket.broadcast.emit('newMessage', {
+    // 	from: msg.from,
+    // 	text: msg.text,
+    // 	createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', ()=>{
     console.log('disconnected form client.'); 	
   })
 
-  socket.emit('newEmail', {
-  	from: 'test@yopmail.com',
-  	text: 'hey how are you',
-  	createdAt: 123
-  });
+  socket.emit('newEmail', generateMessage('test@yopmail.com', 'hey how are you'));
 })
 
 app.use(express.static(publicPath));
